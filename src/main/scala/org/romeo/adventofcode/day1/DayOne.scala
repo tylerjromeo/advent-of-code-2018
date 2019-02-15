16adf34aa03cfb4e3aa878a157fcb1f4c2e3f57e
Tyler Romeo <tyler.romeo@corvesta.com>
2019-02-15 13:43:03 -0500

    day one solution

--- src/main/scala/org/romeo/adventofcode/day1/DayOne.scala
+++ src/main/scala/org/romeo/adventofcode/day1/DayOne.scala
@@ -10,15 +10,31 @@ object DayOne {
 
 class DayOne extends Puzzle("https://adventofcode.com/2018/day/1/input") {
 
-  override def validateInput(input: String): Unit = {
-
+  def parseInput(input: String): List[Int] = {
+    input.split("\n").map(_.toInt).toList
   }
 
+  override def validateInput(input: String): Unit = {
+    assert(parseInput(input).nonEmpty)
+  }
+
+
   override def solvePart1(input: String): String = {
-    ""
+    parseInput(input).sum.toString
   }
 
   override def solvePart2(input: String): String = {
-    ""
+    val inputInts = parseInput(input)
+    val infiniteInput = Stream.continually(inputInts.toStream).flatten
+    val totals = infiniteInput.scan(0)(_ + _)
+    val reduceToMap = totals.scanLeft((Set.empty[Int], 0)) {
+      case ((totalsSoFar, checkTotal), nextTotal) => {
+        (totalsSoFar + checkTotal, nextTotal)
+      }
+    }
+    val setContains: (Set[Int], Int) => Boolean = (xs, x) => xs.contains(x)
+    // drop the first 2 so it doesn't trigger on the first 0 in the list
+    val result = reduceToMap.drop(2).find(setContains.tupled)
+    result.get._2.toString
   }
 }
